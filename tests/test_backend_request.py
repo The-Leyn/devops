bb64865dabe8a00d1d15adea4609a35d5cf421bd
The-Leyn <allard15@hotmail.fr>
2024-12-20 12:00:56 +0100

    Github workflow add push dockerHub

--- tests/test_backend_request.py
+++ tests/test_backend_request.py
@@ -3,6 +3,6 @@ import requests
 def test_getBackendResponse():
     try:
         response = requests.get("http://localhost:3000/tasks")
-                
-    # except requests.exceptions.RequestException as e:
-    #     assert False, f"Erreur de connexion au backend : {str(e)}"
+        
+    except requests.exceptions.RequestException as e:
+        assert False, f"Erreur de connexion au backend : {str(e)}"
